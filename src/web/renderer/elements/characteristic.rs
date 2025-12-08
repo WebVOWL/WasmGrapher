3067ad359c1ad8a668e4571f29126d8b26b6952f
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2025-12-08 08:45:04 +0000

    Add mising characteristics

--- src/web/renderer/elements/characteristic.rs
+++ src/web/renderer/elements/characteristic.rs
@@ -2,6 +2,10 @@ pub enum Characteristic {
     Transitive,
     FunctionalProperty,
     InverseFunctionalProperty,
+    Reflexive,
+    Irreflexive,
+    Symmetric,
+    Asymmetric,
     HasKey,
 }
 
@@ -11,6 +15,10 @@ impl std::fmt::Display for Characteristic {
             Characteristic::Transitive => write!(f, "transitive"),
             Characteristic::FunctionalProperty => write!(f, "functional"),
             Characteristic::InverseFunctionalProperty => write!(f, "inverse functional"),
+            Characteristic::Reflexive => write!(f, "reflexive"),
+            Characteristic::Irreflexive => write!(f, "irreflexive"),
+            Characteristic::Symmetric => write!(f, "symmetric"),
+            Characteristic::Asymmetric => write!(f, "asymmetric"),
             Characteristic::HasKey => write!(f, "key"),
         }
     }
