1286009cd392e459e2a03d7b13597476907f9c1e
khmikkelsen <kh.mikkelsen@hotmail.com>
2026-02-11 20:27:22 +0100

    Derive EnumIter

--- src/renderer/elements/characteristic.rs
+++ src/renderer/elements/characteristic.rs
@@ -1,6 +1,8 @@
 use rkyv::{Archive, Deserialize, Serialize};
+use strum::EnumIter;
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Archive, Deserialize, Serialize)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Archive, Deserialize, Serialize, EnumIter)]
+#[strum(serialize_all = "title_case")]
 pub enum Characteristic {
     Transitive,
     FunctionalProperty,
