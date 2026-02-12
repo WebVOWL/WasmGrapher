1d2f0df2f95c6ecfb71dbd42af2971780bbb401b
Kristian Mikkelsen <kh.mikkelsen@hotmail.com>
2026-02-12 09:56:37 +0000

    fmt::display --> strum::Display

--- src/renderer/elements/characteristic.rs
+++ src/renderer/elements/characteristic.rs
@@ -13,18 +13,3 @@ pub enum Characteristic {
     AsymmetricProperty,
     HasKey,
 }
-
-impl std::fmt::Display for Characteristic {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::Transitive => write!(f, "transitive"),
-            Self::FunctionalProperty => write!(f, "functional"),
-            Self::InverseFunctionalProperty => write!(f, "inverse functional"),
-            Self::ReflexiveProperty => write!(f, "reflexive"),
-            Self::IrreflexiveProperty => write!(f, "irreflexive"),
-            Self::SymmetricProperty => write!(f, "symmetric"),
-            Self::AsymmetricProperty => write!(f, "asymmetric"),
-            Self::HasKey => write!(f, "key"),
-        }
-    }
-}
