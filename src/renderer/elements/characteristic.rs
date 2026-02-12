9d6df2ae6606e7f8eaeac94b46ec5c7bfc190907
khmikkelsen <kh.mikkelsen@hotmail.com>
2026-02-12 11:09:58 +0100

    Derive EnumIter (#55)
    * Derive EnumIter
    
    * Update src/renderer/elements/characteristic.rs
    
    * fmt::display --> strum::Display
    
    * cargo fmt
    
    ---------
    
    Co-authored-by: TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>

--- src/renderer/elements/characteristic.rs
+++ src/renderer/elements/characteristic.rs
@@ -1,6 +1,20 @@
 use rkyv::{Archive, Deserialize, Serialize};
+use strum::EnumIter;
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Archive, Deserialize, Serialize)]
+#[derive(
+    Copy,
+    Clone,
+    Debug,
+    PartialEq,
+    Eq,
+    Hash,
+    Archive,
+    Deserialize,
+    Serialize,
+    EnumIter,
+    strum::Display,
+)]
+#[strum(serialize_all = "title_case")]
 pub enum Characteristic {
     Transitive,
     FunctionalProperty,
@@ -10,19 +24,4 @@ pub enum Characteristic {
     SymmetricProperty,
     AsymmetricProperty,
     HasKey,
-}
-
-impl std::fmt::Display for Characteristic {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::Transitive => write!(f, "transitive"),
-            Self::FunctionalProperty => write!(f, "functional"),
-            Self::InverseFunctionalProperty => write!(f, "inverse functional"),
-            Self::ReflexiveProperty => write!(f, "reflexive"),
-            Self::IrreflexiveProperty => write!(f, "irreflexive"),
-            Self::SymmetricProperty => write!(f, "symmetric"),
-            Self::AsymmetricProperty => write!(f, "asymmetric"),
-            Self::HasKey => write!(f, "key"),
-        }
-    }
 }
