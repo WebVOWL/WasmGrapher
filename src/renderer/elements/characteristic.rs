393d6052cf1db8f3ea79e85050d1fcf68a7f1ec3
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2026-02-12 10:44:00 +0100

    Update src/renderer/elements/characteristic.rs

--- src/renderer/elements/characteristic.rs
+++ src/renderer/elements/characteristic.rs
@@ -1,7 +1,7 @@
 use rkyv::{Archive, Deserialize, Serialize};
 use strum::EnumIter;
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Archive, Deserialize, Serialize, EnumIter)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Archive, Deserialize, Serialize, EnumIter, strum::Display)]
 #[strum(serialize_all = "title_case")]
 pub enum Characteristic {
     Transitive,
