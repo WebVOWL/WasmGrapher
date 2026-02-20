742e5ae0bcd6ef96b9e8faec61c536fb7d278c7a
khmikkelsen <kh.mikkelsen@hotmail.com>
2026-02-20 16:59:58 +0100

    add strum serialize strings, required for https://github.com/WebVOWL/VOWL-R/pull/182 (#65)

--- src/renderer/elements/characteristic.rs
+++ src/renderer/elements/characteristic.rs
@@ -16,12 +16,20 @@ use strum::EnumIter;
 )]
 #[strum(serialize_all = "title_case")]
 pub enum Characteristic {
+    #[strum(serialize = "transitive")]
     Transitive,
+    #[strum(serialize = "functional")]
     FunctionalProperty,
+    #[strum(serialize = "inverse functional")]
     InverseFunctionalProperty,
+    #[strum(serialize = "reflexive")]
     ReflexiveProperty,
+    #[strum(serialize = "irreflexive")]
     IrreflexiveProperty,
+    #[strum(serialize = "symmetric")]
     SymmetricProperty,
+    #[strum(serialize = "asymmetric")]
     AsymmetricProperty,
+    #[strum(serialize = "has key")]
     HasKey,
 }
