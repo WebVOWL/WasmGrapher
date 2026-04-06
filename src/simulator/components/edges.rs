b511290166fcf6d116cfc585a6791a80017bfd37
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2026-04-06 22:09:06 +0200

    Cleanup

--- src/simulator/components/edges.rs
+++ src/simulator/components/edges.rs
@@ -9,28 +9,3 @@ use specs::{Component, Entity, VecStorage};
 pub struct Connects {
     pub targets: Vec<Entity>,
 }
-
-// /// How strong the spring force of an edge should be.
-// #[derive(Component)]
-// #[storage(VecStorage)]
-// pub struct SpringStiffness(pub f32);
-
-// impl Default for SpringStiffness {
-//     fn default() -> Self {
-//         Self(1.0)
-//     }
-// }
-
-// /// Length of an edge in neutral position.
-// ///
-// /// If edge is shorter than neutral it pushers apart.
-// /// If edge is longer than neutral it pulls together.
-// #[derive(Component)]
-// #[storage(VecStorage)]
-// pub struct SpringNeutralLength(pub f32);
-
-// impl Default for SpringNeutralLength {
-//     fn default() -> Self {
-//         Self(2.0)
-//     }
-// }
