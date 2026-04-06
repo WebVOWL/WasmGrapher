29e6aa2e0072dd1ef4402478d0306aec487c1c81
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2026-04-06 22:36:38 +0200

    Fix bench

--- benches/simulator.rs
+++ benches/simulator.rs
@@ -1,6 +1,6 @@
 use criterion::{BenchmarkId, Criterion, criterion_group, criterion_main};
 use glam::Vec2;
-use grapher::prelude::Simulator;
+use grapher::prelude::{ElementType, OwlNode, OwlType, Simulator};
 
 /// Force-directed graph simulation speed
 fn force_directed_simulation(c: &mut Criterion) {
@@ -9,16 +9,18 @@ fn force_directed_simulation(c: &mut Criterion) {
 
     let mut group = c.benchmark_group("Force-directed");
 
+    let mut element_types = Vec::with_capacity(nodes[4] as usize);
     let mut positions = Vec::with_capacity(nodes[4] as usize);
     let mut sizes: Vec<f32> = Vec::with_capacity(nodes[4] as usize);
     for i in nodes {
         positions.clear();
         sizes.clear();
         for _ in 0..i {
+            element_types.push(ElementType::Owl(OwlType::Node(OwlNode::Class)));
             positions.push(Vec2::new(1.0, 1.0));
             sizes.push(1.0);
         }
-        let mut simulator = Simulator::builder().build(&positions, &edges, &sizes);
+        let mut simulator = Simulator::builder().build(&element_types, &positions, &edges, &sizes);
 
         if i >= 3_000_000 {
             group.sample_size(10);
